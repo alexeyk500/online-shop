6703d7f6d1decb973a92e7b895d44f9ac8af6187
alexeyk500 <alexeyk500@yandex.ru>
2022-05-06 11:01:32 +0300

    Models for BD - created

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-require ('dotenv').config();
-const express = require ('express');
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, ()=>{console.log(`Server started on PORT ${PORT}`)})
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server started on PORT ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start().then(() => {});
