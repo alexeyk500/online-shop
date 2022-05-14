c162dd19ca7f2a7ca4faec85edd4e962ce20dc7f
alexeyk500 <alexeyk500@yandex.ru>
2022-05-14 21:33:26 +0300

    checkRoleMiddleware - done

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const brandRouter = new Router();
 const BrandController = require('../controllers/brandController');
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
-brandRouter.post('/', BrandController.create);
+brandRouter.post('/', checkRoleMiddleware('ADMIN'), BrandController.create);
 brandRouter.get('/', BrandController.getAll);
 
 module.exports = brandRouter;
