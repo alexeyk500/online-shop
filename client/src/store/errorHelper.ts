a322d82d4afeff684455ac456f070d90cf2a66e6
alexeyk500 <alexeyk500@yandex.ru>
2022-06-10 11:02:40 +0300

    Error Handler - done

--- client/src/store/errorHelper.ts
+++ client/src/store/errorHelper.ts
@@ -1,9 +1,3 @@
-import { AnyAction } from '@reduxjs/toolkit';
-
-export const isError = (action: AnyAction) => {
-  return action.type.endsWith('rejected');
-};
-
 export const showError = (message: string | undefined) => {
   alert(message ? message : 'Undefined Error');
 };
